d764c599946789ead3da16e28950a845869d8779
greybeetle <hanxuan_g@163.com>
2024-12-08 19:58:33 +0800

    change gitignore

--- docker/instock_base/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd ../../
-
-DOCKER_NAME=instock_base
-TAG=latest
-
-echo " docker build -f docker/instock_base/Dockerfile -t ${DOCKER_NAME} ."
-docker build -f docker/instock_base/Dockerfile -t ${DOCKER_NAME}:${TAG} .
